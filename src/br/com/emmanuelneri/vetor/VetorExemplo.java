997a229537ea6514ed25d818e7ffc899d6bf8e6b
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2018-08-04 19:51:42 -0300

    exemplo com a classe Vector

---
+++ src/br/com/emmanuelneri/vetor/VetorExemplo.java
@@ -0,0 +1,16 @@
+package br.com.emmanuelneri.vetor;
+
+import java.util.Vector;
+
+public class VetorExemplo {
+
+    public static void main(String args[]) {
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+
+        System.out.println(vector);
+
+    }
+
+}
