3862f5e91ce2720650d033170e93988d2dcddeec
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2018-09-03 20:29:02 -0300

    ajuste comentario

--- src/br/com/emmanuelneri/listas/exercicios/ProblemaCombinarDuasListas.java
+++ src/br/com/emmanuelneri/listas/exercicios/ProblemaCombinarDuasListas.java
@@ -24,7 +24,7 @@ public class ProblemaCombinarDuasListas {
         }
 
         System.out.println(Arrays.toString(listaCombinada)); // apresenta da lista combinada
-        // Resultado: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+        // Resultado: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
     }
 
 }
